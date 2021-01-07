3512dee1ce1a9c1139292cb7a8f8f7eb59a68348
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-07 11:13:17 +0100

    [DONE] login page : styling and default route to it

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res, next) => {
-  res.redirect('/auth/signup');
+  res.redirect('/auth/login');
 });
 
 router.get('/signup', (req, res, next) => {
   res.render('auth/signup', { pageTitle: 'Inscription' });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', { pageTitle: 'Connexion' });
 });
 
 module.exports = router;
