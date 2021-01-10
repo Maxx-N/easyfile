67462bb0ff8894f7c8e183fba282eaa90c81497b
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-10 18:30:04 +0100

    [DONE] isAuth middleware, protecting the documents route and displaying the documents link in navbar only if a user is authenticated

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const userController = require('../controllers/user');
+const isAuth = require('../middleware/is-auth');
 
 //
 
 const router = express.Router();
 
-router.get('/documents', userController.getDocuments);
+router.get('/documents', isAuth, userController.getDocuments);
 
 module.exports = router;
