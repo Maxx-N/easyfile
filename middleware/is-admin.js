7b4277aea444bf217153ccd5a69ab5846d583ace
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-11 18:01:08 +0100

    [DONE] admin email correction

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -1,8 +1,7 @@
+const ADMIN_EMAIL = require('../private').ADMIN_EMAIL;
+
 module.exports = (req, res, next) => {
-  if (
-    !req.session.user ||
-    req.session.user.email !== 'max.easyfile@gmail.com'
-  ) {
+  if (!req.session.user || req.session.user.email !== ADMIN_EMAIL) {
     return res.redirect('/');
   }
   next();
