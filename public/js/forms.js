469810b1e50118adacdf1e34b82160bf40ff075b
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-15 09:11:36 +0100

    [DONE] changing dynamically the fields in the form to add a document, depending on the doctype, using front-end javascript

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,4 +1,5 @@
 const options = document.querySelectorAll('option');
+
 for (let option of options) {
   const select = option.parentElement;
   if (select.getAttribute('lastChoice') === option.value) {
@@ -25,3 +26,73 @@ checkboxes.forEach((checkbox) => {
     }
   });
 });
+
+// Form to add a document (display fields depending on the selected doctype)
+
+const doctypeSelect = document.getElementById('doctypeId');
+const doctypeOptions = [...document.querySelectorAll('#doctypeId > option')];
+
+// Field names
+const title = document.getElementById('titleGroup');
+const issuanceDate = document.getElementById('issuanceDateGroup');
+const expirationDate = document.getElementById('expirationDateGroup');
+const month = document.getElementById('monthGroup');
+const year = document.getElementById('yearGroup');
+
+let selectedDoctypeOption = doctypeOptions.find((option) => {
+  return doctypeSelect.value === option.value;
+});
+
+if (!!selectedDoctypeOption.value) {
+  changeFields();
+} else {
+  title.style.display = 'none';
+  issuanceDate.style.display = 'none';
+  expirationDate.style.display = 'none';
+  month.style.display = 'none';
+  year.style.display = 'none';
+}
+
+doctypeSelect.addEventListener('change', changeFields);
+
+function changeFields() {
+  selectedDoctypeOption = doctypeOptions.find((option) => {
+    return doctypeSelect.value === option.value;
+  });
+
+  // Doctype properties
+  const isUnique = selectedDoctypeOption.getAttribute('isUnique') === 'true';
+  const isMonthly =
+    selectedDoctypeOption.getAttribute('periodicity') === 'month';
+  const isYearly = selectedDoctypeOption.getAttribute('periodicity') === 'year';
+  const hasissuanceDate =
+    selectedDoctypeOption.getAttribute('hasissuanceDate') === 'true';
+  const hasExpirationDate =
+    selectedDoctypeOption.getAttribute('hasExpirationDate') === 'true';
+
+  if (isUnique) {
+    title.style.display = 'none';
+  } else {
+    title.style.display = 'block';
+  }
+  if (hasissuanceDate) {
+    issuanceDate.style.display = 'block';
+  } else {
+    issuanceDate.style.display = 'none';
+  }
+  if (hasExpirationDate) {
+    expirationDate.style.display = 'block';
+  } else {
+    expirationDate.style.display = 'none';
+  }
+  if (isMonthly) {
+    month.style.display = 'block';
+  } else {
+    month.style.display = 'none';
+  }
+  if (isYearly) {
+    year.style.display = 'block';
+  } else {
+    year.style.display = 'none';
+  }
+}
