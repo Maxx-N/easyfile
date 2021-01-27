fed6331d186c2e3b702f94c19dece1102c7326f3
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-27 19:47:59 +0100

    [DONE] the pro can create a client when this latter is not registered yet (view pro/edit-client)

--- controllers/pro.js
+++ controllers/pro.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 const LoanFile = require('../models/loan-file');
@@ -68,6 +69,13 @@ exports.postEnterClientEmail = async (req, res, next) => {
       return res.render('pro/edit-client', {
         pageTitle: 'Nouveau client',
         path: '/clients',
+        email: email,
+        oldInput: {
+          password: '',
+          confirmPassword: '',
+        },
+        errorMessages: [],
+        validationErrors: [],
       });
     }
 
@@ -79,6 +87,53 @@ exports.postEnterClientEmail = async (req, res, next) => {
       },
       validationErrors: [],
       errorMessages: [],
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postEditClient = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((err) => {
+      return err.msg;
+    });
+    return res.render('pro/edit-client', {
+      pageTitle: 'Nouveau client',
+      path: '/clients',
+      email: email,
+      oldInput: {
+        password: password,
+        confirmPassword: confirmPassword,
+      },
+      errorMessages: errorMessages,
+      validationErrors: errors.array(),
+    });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+    });
+    await user.save((err) => {
+      if (err) {
+        err.message =
+          "Un problème est survenu sur le serveur et l'utilisateur n'a pas pu être enregistré. Nous travaillons à résoudre le problème et vous prions de bien vouloir nous excuser.";
+        throw err;
+      }
+    });
+
+    res.render('pro/edit-loan-file', {
+      pageTitle: 'Nouveau dossier de prêt',
+      path: '/loan-files',
+      user: user,
     });
   } catch (err) {
     next(err);
--- routes/pro.js
+++ routes/pro.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 
 //
 
@@ -24,6 +24,29 @@ router.post(
       .withMessage('Merci de saisir un e-mail valide.'),
   ],
   proController.postEnterClientEmail
+);
+
+router.post(
+  '/edit-client',
+  isProAuth,
+  [
+    body('password')
+      .trim()
+      .isStrongPassword()
+      .withMessage(
+        'Le mot de passe doit contenir au moins 8 caractères, dont : une minuscule, une majuscule, un chiffre et un symbole.'
+      ),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          return false;
+        }
+        return true;
+      })
+      .withMessage('Les mots de passe saisis doivent être identiques.'),
+  ],
+  proController.postEditClient
 );
 
 module.exports = router;
