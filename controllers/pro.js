b16f57b0065b3639f16be3ec9f2342d7f0d80d3d
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-27 08:14:22 +0100

    [DONE] page enter-client-email to navigate to edit-client if client does not exist yet or to edit-loan-file if he does

--- controllers/pro.js
+++ controllers/pro.js
@@ -1,6 +1,86 @@
+const { validationResult } = require('express-validator');
+
+const User = require('../models/user');
+const LoanFile = require('../models/loan-file');
+
+//
+
 exports.getLoanFiles = (req, res, next) => {
   res.render('pro/loan-files', {
     pageTitle: 'Dossiers de prêt',
     path: '/loan-files',
   });
+};
+
+exports.getEnterClientEmail = (req, res, next) => {
+  res.render('pro/enter-client-email', {
+    pageTitle: 'Dossiers de prêt',
+    path: '/loan-files',
+    oldInput: {
+      email: '',
+    },
+    validationErrors: [],
+    errorMessages: [],
+  });
+};
+
+exports.postEnterClientEmail = async (req, res, next) => {
+  const email = req.body.email;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((err) => {
+      return err.msg;
+    });
+    return res.render('pro/enter-client-email', {
+      pageTitle: 'Dossiers de prêt',
+      path: '/loan-files',
+      oldInput: {
+        email: email,
+      },
+      validationErrors: errors.array(),
+      errorMessages: errorMessages,
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+      // const loanFile = new LoanFile({
+      //   userId: user._id,
+      //   proId: req.pro._id,
+      //   status: 'pending',
+      // });
+      // await loanFile.save();
+      // user.loanFileIds.push(loanFile._id);
+      // await user.save();
+      // await loanFile.populate('userId', '-documentIds -loanFileIds').execPopulate();
+
+      return res.render('pro/edit-loan-file', {
+        pageTitle: 'Nouveau dossier de prêt',
+        path: '/loan-files',
+        user: user,
+      });
+    }
+
+    if (!user) {
+      return res.render('pro/edit-client', {
+        pageTitle: 'Nouveau client',
+        path: '/clients',
+      });
+    }
+
+    res.render('pro/enter-client-email', {
+      pageTitle: 'Dossiers de prêt',
+      path: '/loan-files',
+      oldInput: {
+        email: '',
+      },
+      validationErrors: [],
+      errorMessages: [],
+    });
+  } catch (err) {
+    next(err);
+  }
 };
--- routes/pro.js
+++ routes/pro.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const {body} = require('express-validator');
+
+//
 
 const proController = require('../controllers/pro');
 const isProAuth = require('../middleware/is-pro-auth');
@@ -7,6 +10,20 @@ const isProAuth = require('../middleware/is-pro-auth');
 
 const router = express.Router();
 
-router.get('/loan-files', isProAuth ,proController.getLoanFiles);
+router.get('/loan-files', isProAuth, proController.getLoanFiles);
+
+router.get('/enter-client-email', isProAuth, proController.getEnterClientEmail);
+
+router.post(
+  '/enter-client-email',
+  isProAuth,
+  [
+    body('email')
+      .normalizeEmail({ gmail_remove_dots: false })
+      .isEmail()
+      .withMessage('Merci de saisir un e-mail valide.'),
+  ],
+  proController.postEnterClientEmail
+);
 
 module.exports = router;
