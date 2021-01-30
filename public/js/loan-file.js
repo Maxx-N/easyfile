5446737fbec28e71f8224e0611e54d3cc718c693
Maxime NORVAL <mnorval@groupe-isia.com>
2021-01-30 12:23:52 +0100

    [DONE] correcting front js errors by creating a specific file for edit-document

--- public/js/loan-file.js
+++ public/js/loan-file.js
@@ -4,7 +4,11 @@ button.addEventListener('click', () => {
   const form = document.getElementById('deletingForm');
   const clientEmail = form.getAttribute('clientEmail');
 
-  if (confirm(`Êtes-vous sûr(e) de vouloir supprimer le dossier de prêt de votre client ${clientEmail} ?`)) {
+  if (
+    confirm(
+      `Êtes-vous sûr(e) de vouloir supprimer le dossier de prêt de votre client ${clientEmail} ?`
+    )
+  ) {
     form.submit();
   }
 });
